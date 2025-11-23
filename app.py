95b7f576150b301b321cc550427e88b143d155c1
Siju Wilson <sijuwilson07@gmail.com>
2025-11-23 14:35:39 +0530

    Refactor app.py to improve structure and readability

--- app.py
+++ app.py
@@ -330,11 +330,12 @@ def chat():
 
 if __name__ == "__main__":
     with app.app_context():
-        db.create_all():
+        db.create_all()
     
     # Run the app
     from os import environ
     port = int(environ.get('PORT', 5000))
 
     app.run(host='0.0.0.0', port=port, debug=True)
+
 
